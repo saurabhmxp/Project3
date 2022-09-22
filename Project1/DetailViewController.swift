fd8f105c1ea8d976aa256f7c584a37e28c20628d
Saurabh <saurabh.agarwal@mxplayer.in>
2022-09-22 10:15:21 +0530

    added a detail view for viewing image

---
+++ Project1/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Saurabh Agarwal on 22/09/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    //IBOutlet attribute tells xcode that there's a connection between this line of code and something in interface builder
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
