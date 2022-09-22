747f709c2b3bfc624962772071f404559b2e73b1
Saurabh <saurabh.agarwal@mxplayer.in>
2022-09-22 16:30:24 +0530

    adding share button to individual photo and saving them to the simulator

--- Project3/DetailViewController.swift
+++ Project3/DetailViewController.swift
@@ -21,6 +21,8 @@ class DetailViewController: UIViewController {
         
         navigationItem.largeTitleDisplayMode = .never
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage{
             imageView.image = UIImage(named: imageToLoad)
         }
@@ -34,6 +36,17 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped(){
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image Found")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+        
     }
     
 
